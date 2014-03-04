9781dde1054680fc55cdcc4e739aa93fdf2545bf
Ivanyushenko <vladnick200808@gmail.com>
2014-03-04 17:28:10 +0400

    1234

---
+++ MyProject/MyProject/MyProject/puz.cpp
@@ -0,0 +1,19 @@
+#include "head.h"
+
+void PuzSort(char *mas, int n)///coment function PuzSort
+{
+	int i,j;
+	char c;
+	for ( i = (n - 1); i >= 0; i--)
+	{
+		for ( j = 0; j < i; j++)
+		{
+			if ( mas[j] > mas[j+1] ) 
+			{
+				c = mas[j];
+				mas[j] = mas[j+1];
+				mas[j+1] = c;
+			}
+		}
+	}
+}
