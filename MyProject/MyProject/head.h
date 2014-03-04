f6fc3e5d210d9088c5ed3891c363597dae864741
Ivanyushenko <vladnick200808@gmail.com>
2014-03-04 17:17:54 +0400

    123

---
+++ MyProject/MyProject/head.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <time.h>
+#include <iostream>
+
+void PuzSort(char *mas, int n);
+void Merge(char *A, int first, int last);
+void MergeSort(char *A, int first, int last);
